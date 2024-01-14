79679c735a511e8313c2f9b56e30b123ad3dd085
WilliamOnyejiaka <williamonyejiaka08062528003@gmail.com>
2024-01-14 02:45:48 +0100

    add port variable to the database class

--- lib/Database.php
+++ lib/Database.php
@@ -4,27 +4,32 @@ namespace Lib;
 
 ini_set('display_errors', 1);
 
-class Database {
+class Database
+{
 
   private $hostname;
   private $db_name;
   private $username;
   private $password;
+  private $port;
 
-  public function __construct(string $hostname,string $username,string $password,string $db_name){
+  public function __construct(string $hostname, string $username, string $password, string $db_name, int $port=3306)
+  {
     $this->hostname = $hostname;
     $this->username = $username;
     $this->password = $password;
     $this->db_name = $db_name;
-
+    $this->port = $port;
   }
 
-  public function connect(){
-    $conn = new \mysqli($this->hostname,$this->username,$this->password,$this->db_name);
-    if($conn->connect_errno){
+  public function connect()
+  {
+    $conn = new \mysqli($this->hostname, $this->username, $this->password, $this->db_name,$this->port);
+    if ($conn->connect_errno) {
       print_r($conn->error);
       exit;
-    }else {return $conn;}
+    } else {
+      return $conn;
+    }
   }
 }
-?>
